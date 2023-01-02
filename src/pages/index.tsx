0cc5a426f2775df03124288a9c02b60f7dc14847
Jye Lewis <jye@jyelewis.com>
2023-01-03 08:32:46 +1100

    Added external posts to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./index.scss";
 import { graphql, Link } from "gatsby";
 import { Layout } from "../components/Layout";
 import { dateFromSlug } from "../utilities/dateFromSlug";
 import profileImage from "../images/profile-image.jpeg";
 
+const externalPosts = [
+  {
+    id: "external-1",
+    url: "https://medium.com/life-at-propeller/oops-i-did-it-again-435f21f6c739",
+    slug: "2022-05-19",
+    frontmatter: {
+      title: "Oops, I did it again!",
+      description:
+        "The grass is always greener: Do I want to work as an IC or lead?",
+      timeToRead: 6,
+    },
+  },
+  {
+    id: "external-2",
+    url: "https://medium.com/life-at-propeller/hackathon-driven-development-taking-dirtlapse-to-production-55683d9691bb",
+    slug: "2022-09-15",
+    frontmatter: {
+      title: "Hackathon Driven Development",
+      description: "Taking 'Dirtlapse' from a hackathon project to production",
+      timeToRead: 6,
+    },
+  },
+];
+
 export const Index: React.FC<any> = ({ data }) => {
+  const allPosts = useMemo(() => {
+    const posts = externalPosts.concat(data.allMdx.nodes);
+    posts.sort((a, b) => b.slug.localeCompare(a.slug));
+
+    return posts;
+  }, [data.allMdx.nodes]);
+
   return (
     <Layout title="Posts" description="">
       <div className="page-index">
@@ -17,9 +48,12 @@ export const Index: React.FC<any> = ({ data }) => {
           <div style={{ clear: "both" }} />
         </div>
 
-        {data.allMdx.nodes.map((node: any) => (
+        {allPosts.map((node: any) => (
           <article key={node.id}>
-            <Link to={`/blog/${node.slug}`}>
+            <Link
+              to={node.url || `/blog/${node.slug}`}
+              target={node.url ? "_blank" : undefined}
+            >
               <h2 className="title">{node.frontmatter.title}</h2>
               <p className="date">
                 {node.frontmatter.timeToRead || node.timeToRead} min read.{" "}
